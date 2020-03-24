ed1a26c46885de6e03629f98ee23ed6bad64447b
aTewan <tewan33@gmail.com>
2020-03-24 14:49:01 +0100

    :fire: Suppression de fichiers qui ont été compilés par erreur :///

--- api/src/users/user.model.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var UserGender;
-(function (UserGender) {
-    UserGender["FEMALE"] = "FEMALE";
-    UserGender["MALE"] = "MALE";
-})(UserGender = exports.UserGender || (exports.UserGender = {}));
