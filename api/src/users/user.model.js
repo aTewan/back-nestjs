cedc4db4e5884e92e500834432be0c56c66cb6cd
aTewan <tewan33@gmail.com>
2020-03-24 14:35:31 +0100

    :truck: Déplacement de fichiers

---
+++ api/src/users/user.model.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var UserGender;
+(function (UserGender) {
+    UserGender["FEMALE"] = "FEMALE";
+    UserGender["MALE"] = "MALE";
+})(UserGender = exports.UserGender || (exports.UserGender = {}));
