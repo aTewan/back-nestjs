11210e9206b79ac892f9e838a3b1a5d18663ecec
aTewan <tewan33@gmail.com>
2020-03-25 15:31:41 +0100

    :pencil: Ajout de décorateurs pour la documentation Swagger

--- api/src/users/user.model.ts
+++ api/src/users/user.model.ts
@@ -1,14 +1,25 @@
 import { Document } from 'mongoose'
-
-export interface User extends Document {
-  id: string;
-  firstname: string;
-  lastname: string;
-  email: string;
-  gender: UserGender;
-}
+import { ApiProperty } from '@nestjs/swagger';
 
 export enum UserGender {
   FEMALE = "FEMALE",
   MALE = "MALE"
 }
+
+export class User extends Document {
+  @ApiProperty({ example: "5e7b6a3805f61e0058449d37", description: "Identifiant en BDD de l'utilisateur" })
+  _id: string;
+
+  @ApiProperty({ example: "John", description: "Prénom de l'utilisateur"})
+  firstname: string;
+
+  @ApiProperty({ example: "Doe", description: "Nom de famille de l'utilisateur"})
+  lastname: string;
+
+  @ApiProperty({ example: "john.doe@test.com", description: "E-mail de l'utilisateur"})
+  email: string;
+
+  @ApiProperty({ example: "MALE", description: "Genre de l'utilisateur", enum: UserGender})
+  gender: UserGender;
+}
+
--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Param, Delete, UsePipes, ValidationPipe } 
 import { UsersService } from './users.service';
 import { User, UserGender } from './user.model'
 import { CreateUserDto } from './dto/create-user-dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiCreatedResponse, ApiNotFoundResponse, ApiAcceptedResponse, ApiOkResponse } from '@nestjs/swagger';
 
 @ApiTags('users')
 @Controller('users')
@@ -10,22 +10,28 @@ export class UsersController {
   constructor(private usersServices: UsersService) {}
 
   @Get()
+  @ApiOkResponse({description: "Appel à la base de donnée réussie", type: [User]})
   async getAllUsers(): Promise<User[]> {
     return await this.usersServices.getAllUsers();
   }
   
   @Get('/:id')
+  @ApiOkResponse({description: "Un utilisateur a été trouvé", type: User})
+  @ApiNotFoundResponse({description: "Aucun utilisateur trouvé"})
   async getUserById(@Param('id') id: string): Promise<User> {
     return await this.usersServices.getUserById(id);
   }
 
   @Delete('/:id')
+  @ApiOkResponse({description: "Un utilisateur a été supprimé", type: User})
+  @ApiNotFoundResponse({description: "Aucun utilisateur trouvé"})
   async deleteUserById(@Param('id') id: string): Promise<User> {
     return await this.usersServices.deleteUserById(id);
   }
   
   @Post()
   @UsePipes(ValidationPipe)
+  @ApiCreatedResponse({description: "L'utilisateur a bien été enregistré", type: User})
   async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
     return await this.usersServices.createUser(createUserDto);
   }
